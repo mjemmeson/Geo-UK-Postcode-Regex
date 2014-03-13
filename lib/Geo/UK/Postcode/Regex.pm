61c5b0a653b810d3bc28eafa35ee367af6a8db0b
Michael Jemmeson <mjemmeson@cpan.org>
2014-03-13 10:49:48 +0000

    adding SUPPORT back into POD

--- lib/Geo/UK/Postcode/Regex.pm
+++ lib/Geo/UK/Postcode/Regex.pm
@@ -599,6 +599,23 @@ L<Form::Validator::UKPostcode>
 
 =back
 
+=head1 SUPPORT
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests through the issue tracker
+at L<https://github.com/mjemmeson/geo-uk-postcode-regex/issues>.
+You will be notified automatically of any progress on your issue.
+
+=head2 Source Code
+
+This is open source software.  The code repository is available for
+public review and contribution under the terms of the license.
+
+L<https://github.com/mjemmeson/geo-uk-postcode-regex>
+
+    git clone git://github.com/mjemmeson/geo-uk-postcode-regex.git
+
 =head1 AUTHOR
 
 Michael Jemmeson E<lt>mjemmeson@cpan.orgE<gt>
